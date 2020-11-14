29b2473cb2df3b6d4c6b97f4f96bcfc55139a126
Athul-R-Ashok <70190516+Athul-R-Ashok@users.noreply.github.com>
2020-11-14 08:00:42 +0530

    labcycle

---
+++ Hello_World_PHP_Lab_Cycle_Three/hello_world.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            HELLO WORLD IN PHP
+        </title>
+    </head>
+    <body>
+        <?php
+        echo "HELLO WORLD";  
+        ?>
+   </body>
+</html>
